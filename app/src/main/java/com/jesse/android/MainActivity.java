c91438c96f509583325d78b58cfe6a668a38c7fb
QDPeng <liusp_008@163.com>
2015-09-05 20:02:15 +0800

    Initial commit

---
+++ app/src/main/java/com/jesse/android/MainActivity.java
@@ -0,0 +1,152 @@
+package com.jesse.android;
+
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.jesse.android.material.SlidingTabLayout;
+import com.jesse.android.material.ViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
+    private ListView mDrawerList;
+    private ViewPager pager;
+    private String titles[] = new String[]{"Tab 1", "Tab 2", "Tab 3"};
+    private Toolbar toolbar;
+    private SlidingTabLayout slidingTabLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.navdrawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            toolbar.setNavigationIcon(R.mipmap.ic_ab_drawer);
+        }
+        pager = (ViewPager) findViewById(R.id.viewpager);
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        pager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager(), titles));
+
+        slidingTabLayout.setViewPager(pager);
+        slidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return Color.WHITE;
+            }
+        });
+        drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        mDrawerLayout.setDrawerListener(drawerToggle);
+        String[] values = new String[]{
+                "DEFAULT", "RED", "BLUE", "MATERIAL GREY"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        mDrawerList.setBackgroundColor(getResources().getColor(R.color.material_deep_teal_500));
+                        toolbar.setBackgroundColor(getResources().getColor(R.color.material_deep_teal_500));
+                        slidingTabLayout.setBackgroundColor(getResources().getColor(R.color.material_deep_teal_500));
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case 1:
+                        mDrawerList.setBackgroundColor(getResources().getColor(R.color.red));
+                        toolbar.setBackgroundColor(getResources().getColor(R.color.red));
+                        slidingTabLayout.setBackgroundColor(getResources().getColor(R.color.red));
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+                        break;
+                    case 2:
+                        mDrawerList.setBackgroundColor(getResources().getColor(R.color.blue));
+                        toolbar.setBackgroundColor(getResources().getColor(R.color.blue));
+                        slidingTabLayout.setBackgroundColor(getResources().getColor(R.color.blue));
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+                        break;
+                    case 3:
+                        mDrawerList.setBackgroundColor(getResources().getColor(R.color.material_blue_grey_800));
+                        toolbar.setBackgroundColor(getResources().getColor(R.color.material_blue_grey_800));
+                        slidingTabLayout.setBackgroundColor(getResources().getColor(R.color.material_blue_grey_800));
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+                        break;
+                }
+
+            }
+        });
+
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifesst.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
